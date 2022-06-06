57df050a0d92ff279d7ec0a0ed06f1ac55aa27fb
GedMarc <marc.magon@gmail.com>
2022-06-07 01:14:33 +0200

    Angular client split and cleanup of base typescript

--- src/test/java/com/jwebmp/plugins/jquery/JQueryPageConfiguratorTest.java
+++ src/test/java/com/jwebmp/plugins/jquery/JQueryPageConfiguratorTest.java
@@ -1,10 +1,5 @@
 package com.jwebmp.plugins.jquery;
 
-import com.jwebmp.core.base.angular.services.annotations.angularconfig.*;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-
 public class JQueryPageConfiguratorTest
 {
 
